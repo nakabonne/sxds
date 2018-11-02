801efb8f4d6db04c683f264fb325a8fcfb138866
nakabonne <rodriguez.nak@gmail.com>
2018-11-02 16:16:11 +0900

    Add Run method to cacher

--- server/cacher/server.go
+++ server/cacher/server.go
@@ -2,8 +2,11 @@ package cacher
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 
 	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	"github.com/julienschmidt/httprouter"
 	"github.com/nakabonne/sxds/config"
 	"go.uber.org/zap"
 )
@@ -24,4 +27,29 @@ func NewServer(ctx context.Context, sc cache.SnapshotCache, conf *config.Cacher,
 		conf:          conf,
 		logger:        l,
 	}
+}
+
+func (s *Server) Run() {
+	router := httprouter.New()
+	conf, err := config.New()
+	if err != nil {
+		panic(err)
+	}
+
+	defer s.logger.Sync()
+
+	router.PUT("/resources/:node_type", putResources)
+	go func(p int, r *httprouter.Router) {
+		if err := http.ListenAndServe(fmt.Sprintf(":%d", p), r); err != nil {
+			s.logger.Error("cacher server closed", zap.Error(err))
+		}
+	}(conf.Cacher.Port, router)
+
+	s.logger.Info("cacher server is listening",
+		zap.Any("conf", conf.Cacher),
+	)
+	return
+}
+
+func putResources(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
