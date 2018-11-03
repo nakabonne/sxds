075b0f36aa558920292c4db5609313ed09c64fa4
nakabonne <rodriguez.nak@gmail.com>
2018-11-03 11:28:35 +0900

    Add method to create snapshot

--- server/cacher/cacher.go
+++ server/cacher/cacher.go
@@ -10,18 +10,32 @@ type cacher struct {
 }
 
 func (c *cacher) setSnapshot(nodeType domain.NodeType, resources *domain.Resources) error {
-	snapshot, err := c.newSnapshot(nodeType, resources)
-	if err != nil {
-		return err
-	}
-	err = c.snapshotCache.SetSnapshot(nodeType, *snapshot)
+	snapshot := c.newSnapshot(nodeType, resources)
+	err := c.snapshotCache.SetSnapshot(nodeType, *snapshot)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *cacher) newSnapshot(nodeType domain.NodeType, resources *domain.Resources) (*cache.Snapshot, error) {
-	// FIXME: 実装する
-	return &cache.Snapshot{}, nil
+func (s *cacher) newSnapshot(nodeType domain.NodeType, resources *domain.Resources) *cache.Snapshot {
+	endpoints := make([]cache.Resource, 0, len(resources.Endpoints))
+	for _, e := range resources.Endpoints {
+		endpoints = append(endpoints, e)
+	}
+	clusters := make([]cache.Resource, 0, len(resources.Clusters))
+	for _, c := range resources.Clusters {
+		clusters = append(clusters, c)
+	}
+	routes := make([]cache.Resource, 0, len(resources.Routes))
+	for _, r := range resources.Routes {
+		routes = append(routes, r)
+	}
+	listeners := make([]cache.Resource, 0, len(resources.Listeners))
+	for _, l := range resources.Listeners {
+		listeners = append(listeners, l)
+	}
+
+	snapshot := cache.NewSnapshot(resources.Version, endpoints, clusters, routes, listeners)
+	return &snapshot
 }
